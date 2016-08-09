809163642b09a42d7e749826487ebb5ab590013c
Mackenzie Boudreau <mackenzieboudreau@Mackenzies-iMac.local>
2016-08-09 14:14:34 -0300

    Started video functionality

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -6,18 +6,38 @@
 //  Copyright © 2016 Mackenzie Boudreau. All rights reserved.
 //
 
+/* Got set up with code from http://binarymosaic.com/custom-video-player-for-ios-with-avfoundation/ */
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let player = AVPlayer()
+    var previewLayer: AVPlayerLayer?
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blackColor()
+        // Set up player
+        let url = NSURL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")
+        let playerItem = AVPlayerItem(URL: url!)
+        player.replaceCurrentItemWithPlayerItem(playerItem)
+        
+        // Set up preview layer
+        previewLayer = AVPlayerLayer(player: player)
+        view.layer.insertSublayer(previewLayer!, atIndex: 0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        player.play()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        previewLayer?.frame.size.height = self.view.bounds.height / 3
+        previewLayer?.frame.size.width = self.view.bounds.width
     }
 
 
